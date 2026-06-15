60ee9a3d4e952267b8cac1809fe5b12216961537
Deepak Verma <dpakverma789@gmail.com>
2026-06-15 09:40:36 +0530

    debug mode

---
+++ restrict_debug_mode/__manifest__.py
@@ -0,0 +1,40 @@
+{
+    "name": "Restrict Debug Mode",
+    "version": "18.0.1.0.0",
+    "category": "Administration",
+    "summary": "Control access to Odoo Developer Mode",
+    "description": """
+Restrict Debug Mode
+
+This module allows administrators to control which users can
+enable or disable Odoo Developer Mode.
+
+Features:
+- Restrict access to Developer Mode.
+- Restrict access to Assets Debug Mode.
+- Restrict access to Test Debug Mode.
+- Permission-based access through security groups.
+- Compatible with Odoo 18.
+
+Only users assigned to the "Allow Debug Mode" group can
+manage debug modes.
+    """,
+    "author": "Deepak Verma",
+    "maintainer": "Deepak Verma",
+    "company": "Deecoders",
+    "website": "https://www.linkedin.com/in/deepak-verma/",
+    "support": "dpakverma789@gmail.com",
+    "license": "LGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "security/restricted_debug_group.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ restrict_debug_mode/models/ir_http.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+from odoo.http import request
+from odoo.tools.misc import str2bool
+from odoo.addons.web.models.ir_http import ALLOWED_DEBUG_MODES  # ['', '1', 'assets', 'tests']
+
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _handle_debug(cls):
+        """
+        Gate enabling debug modes behind a group, without assuming a logged user
+        is already available on request.env.user (which can be empty here).
+        """
+        debug_param = request.httprequest.args.get("debug")
+        if debug_param is not None:
+            requested = [m.strip() for m in (debug_param or "").split(",")]
+            wants_enable = any(
+                (m in ALLOWED_DEBUG_MODES and m != "") or str2bool(m, m)
+                for m in requested
+            )
+            if wants_enable:
+                uid = request.session.uid
+                allowed = False
+                if uid:
+                    user = request.env["res.users"].sudo().browse(uid)
+                    if user and user.exists():
+                        allowed = user.has_group("restrict_debug_mode.group_enable_debug")
+                if not allowed:
+                    request.session.debug = ""
+                    return
+
+        return super()._handle_debug()
